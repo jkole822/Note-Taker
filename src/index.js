44b6530225909cb5ea7794e1809a197eba5b2c53
Kole Gasior <jkole822@gmail.com>
2021-01-07 15:33:36 -0500

    edit spacing, export server

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ const path = require("path");
 
 const app = express();
 const PORT = 3000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const publicDirPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirPath));
 
-require("./routes/html-routes")(app);
 require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
 });
+
+module.exports = app;
